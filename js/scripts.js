6ebb32890aa65a92dc9230708133dd60af5a8cdb
nmhdz <nmontana.cf@gmail.com>
2021-05-16 17:30:42 -0600

    Update scripts.js
    added a "for" loop to the pokemonList array and added indentation to file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 let pokemonList = [
-   {name: "Bulbasaur",
-    height: 7,
-    types: ['grass', 'poison']},
+  {name: "Bulbasaur",
+   height: 0.7,
+   types: ['grass', 'poison']},
 
-   {name: "Venusaur",
-    height: 2,
-    types: ['grass', 'poison']},
+  {name: "Venusaur",
+   height: 2,
+   types: ['grass', 'poison']},
 
-   {name: "Weedle",
-    height: 3,
-    types: ['bug', 'poison']}
+  {name: "Weedle",
+   height: 0.3,
+   types: ['bug', 'poison']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1) {
+    console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " -Wow, that\'s big!");
+  }
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br> ");
+};
