5747df00c1bb5a0aa8593a311ab2d50d5ae54902
nmhdz <nmontana.cf@gmail.com>
2021-05-17 11:39:22 -0600

    Update scripts.js
    added if else statement and implemented paragraphs inside loop

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,8 @@ let pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 1) {
-    console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " -Wow, that\'s big!");
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " -Wow, that\'s big!" + "</p>");
+  }else {
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "</p>");
   }
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br> ");
 };
