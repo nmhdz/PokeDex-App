0f436951fbf1cc188780dbbdfe2ffaa60ec3f439
nmhdz <nmontana.cf@gmail.com>
2021-05-21 20:42:29 -0600

    Update scripts.js
    created a new variable (pokemonRepository) to hold an IIFE that wraps around pokemonList array to prevent accidently accessing it's global state. The IFFE will now return an object with two keys, "add" (used to add an item to the array) and "getAll"(returns pokemonList array). Updated the forEach loop to cope with these changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
-let pokemonList = [
-  {name: "Bulbasaur",
-   height: 0.7,
-   types: ['grass', 'poison']},
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Bulbasaur",
+     height: 0.7,
+     types: ['grass', 'poison']},
 
-  {name: "Venusaur",
-   height: 2,
-   types: ['grass', 'poison']},
+    {name: "Venusaur",
+     height: 2,
+     types: ['grass', 'poison']},
 
-  {name: "Weedle",
-   height: 0.3,
-   types: ['bug', 'poison']}
-];
+    {name: "Weedle",
+     height: 0.3,
+     types: ['bug', 'poison']}
+  ];
+
+  function getAll(){
+    return pokemonList;
+}
+
+  function add(pokemon){
+    pokemonList.push(pokemon);
+}
+
+  return {
+    getAll: getAll,
+    add: add
+};
+})();
 
 
 function myLoopFunction(pokemon){
   document.write("<p>" + pokemon.name + " " + " (height: " + pokemon.height + ")" + " " + "(type: " + pokemon.types + ")" +"</p>");
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
